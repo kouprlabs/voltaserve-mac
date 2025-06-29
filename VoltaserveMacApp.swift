7575e526344331fc7cda7578c1df58127ed8e708
Anass Bouassaba <ebouassaba@gmail.com>
2025-06-29 16:02:51 +0200

    refactor: use views from VoltaserveCore package (#2)

---
+++ VoltaserveMacApp.swift
@@ -0,0 +1,30 @@
+// Copyright (c) 2025 Anass Bouassaba.
+//
+// Use of this software is governed by the Business Source License
+// included in the file LICENSE in the root of this repository.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the GNU Affero General Public License v3.0 only, included in the file
+// AGPL-3.0-only in the root of this repository.
+
+import SwiftUI
+import VoltaserveCore
+
+@available(macOS 15.0, *)
+@main
+struct VoltaserveMacApp: App {
+    @Environment(\.modelContext) private var context
+    @Environment(\.openWindow) private var openWindow
+    @Environment(\.dismissWindow) private var dismissWindow
+    @StateObject private var sessionStore = SessionStore()
+
+    var body: some Scene {
+        voltaserveMac(
+            context: context,
+            openWindow: openWindow,
+            dismissWindow: dismissWindow,
+            sessionStore: sessionStore
+        )
+    }
+}
