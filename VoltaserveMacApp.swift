9e885b52bed4f6a2acbd5021b3921c6bd58d6e3b
Anass Bouassaba <ebouassaba@gmail.com>
2025-07-03 13:30:24 +0200

    chore: update VoltaserveCore package

--- VoltaserveMacApp.swift
+++ VoltaserveMacApp.swift
@@ -18,13 +18,19 @@ struct VoltaserveMacApp: App {
     @Environment(\.openWindow) private var openWindow
     @Environment(\.dismissWindow) private var dismissWindow
     @StateObject private var sessionStore = SessionStore()
+    @StateObject private var workspaceStore = WorkspaceStore()
+    @StateObject private var groupStore = GroupStore()
+    @StateObject private var organizationStore = OrganizationStore()
 
     var body: some Scene {
         voltaserveMac(
             context: context,
             openWindow: openWindow,
             dismissWindow: dismissWindow,
-            sessionStore: sessionStore
+            sessionStore: sessionStore,
+            workspaceStore: workspaceStore,
+            groupStore: groupStore,
+            organizationStore: organizationStore
         )
     }
 }
